4fb75d3999d68830cdc75fb85346cf3f88f12a7b
antbaobao <2929712050@qq.com>
2020-04-17 18:13:20 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,18 +4,23 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import { Button, Input, Pagination, Dropdown, DropdownMenu, DropdownItem } from 'element-ui'
+import { Button, Input, Pagination, Dropdown, DropdownMenu, DropdownItem, Form, FormItem, Radio, RadioGroup, Dialog } from 'element-ui'
 import 'font-awesome/scss/font-awesome.scss'
 import './index.css'
 import '../theme/index.css'
 import customCompoents from '@/components/index'
-Vue.prototype.$ELEMENT = { size: 'small' }
+Vue.prototype.$ELEMENT = { size: 'medium' }
 Vue.use(Button)
 Vue.use(Input)
 Vue.use(Pagination)
 Vue.use(Dropdown)
 Vue.use(DropdownMenu)
 Vue.use(DropdownItem)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(Dialog)
 Vue.use(customCompoents)
 Vue.config.productionTip = false
 
