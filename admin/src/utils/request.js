c2d4759c56a03704197784bfe5ff1b20e805287e
antbaobao <2929712050@qq.com>
2020-07-26 17:35:51 +0800

    .

--- admin/src/utils/request.js
+++ admin/src/utils/request.js
@@ -1,84 +1,84 @@
-import axios from "axios";
-import queryString from "query-string";
+import axios from 'axios'
+import queryString from 'query-string'
 
-axios.defaults.timeout = 5000;
+axios.defaults.timeout = 5000
 
 const HTTP_HEADER = {
-    "Content-Type": "application/json",
-};
-
-class Request {
-    async get(url, params) {
-        url = params ? `${url}?${queryString.stringify(params)}` : `${url}`;
-
-        let config = {
-            url: `${process.env.GATEWAY}${url}`,
-            method: "get",
-            headers: { ...HTTP_HEADER },
-        };
-        if (localStorage.getItem("token")) {
-            config.headers["authorization"] = `Bearer ${localStorage.getItem(
-                "token"
-            )}`;
-        }
-        const result = await axios(config);
-        return result;
-    }
-
-    async post(url, params) {
-        let config = {
-            method: "post",
-            url: process.env.GATEWAY + url,
-            headers: HTTP_HEADER,
-            data: JSON.stringify(params),
-        };
-
-        if (localStorage.getItem("token")) {
-            config.headers["authorization"] = `Bearer ${localStorage.getItem(
-                "token"
-            )}`;
-        }
-
-        const result = await axios(config);
-        return result;
-    }
-
-    async put(url, params) {
-        let config = {
-            method: "put",
-            url: process.env.GATEWAY + url,
-            headers: HTTP_HEADER,
-            data: JSON.stringify(params),
-        };
-
-        if (localStorage.getItem("token")) {
-            config.headers["authorization"] = `Bearer ${localStorage.getItem(
-                "token"
-            )}`;
-        }
-
-        const result = await axios(config);
-        return result;
-    }
-
-    async delete(url, params) {
-        let config = {
-            method: "delete",
-            url: process.env.GATEWAY + url,
-            headers: HTTP_HEADER,
-            data: JSON.stringify(params),
-        };
-
-        if (localStorage.getItem("token")) {
-            config.headers["authorization"] = `Bearer ${localStorage.getItem(
-                "token"
-            )}`;
-        }
-
-        const result = await axios(config);
-        return result;
-    }
+  'Content-Type': 'application/json'
 }
 
-const _ = new Request();
-export default _;
+class Request {
+  async get(url, params) {
+    url = params ? `${url}?${queryString.stringify(params)}` : `${url}`
+
+    let config = {
+      url: `${process.env.GATEWAY}${url}`,
+      method: 'get',
+      headers: { ...HTTP_HEADER }
+    }
+    if (localStorage.getItem('token')) {
+      config.headers['authorization'] = `Bearer ${localStorage.getItem(
+        'token'
+      )}`
+    }
+    const result = await axios(config)
+    return result
+  }
+
+  async post(url, params) {
+    let config = {
+      method: 'post',
+      url: process.env.GATEWAY + url,
+      headers: HTTP_HEADER,
+      data: JSON.stringify(params)
+    }
+
+    if (localStorage.getItem('token')) {
+      config.headers['authorization'] = `Bearer ${localStorage.getItem(
+        'token'
+      )}`
+    }
+
+    const result = await axios(config)
+    return result
+  }
+
+  async put(url, params) {
+    let config = {
+      method: 'put',
+      url: process.env.GATEWAY + url,
+      headers: HTTP_HEADER,
+      data: JSON.stringify(params)
+    }
+
+    if (localStorage.getItem('token')) {
+      config.headers['authorization'] = `Bearer ${localStorage.getItem(
+        'token'
+      )}`
+    }
+
+    const result = await axios(config)
+    return result
+  }
+
+  async delete(url, params) {
+    let config = {
+      method: 'delete',
+      url: process.env.GATEWAY + url,
+      headers: HTTP_HEADER,
+      data: JSON.stringify(params)
+    }
+
+    if (localStorage.getItem('token')) {
+      config.headers['authorization'] = `Bearer ${localStorage.getItem(
+        'token'
+      )}`
+    }
+
+    const result = await axios(config)
+    return result
+  }
+}
+
+const _ = new Request()
+export default _
